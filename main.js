5401de9081e0b53bfcabba470264a85a45f26a30
Lupo Montero <lupomontero@gmail.com>
2025-09-19 22:31:38 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,132 @@
+import { Map, View } from 'ol';
+import Control from 'ol/control/Control';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import { circular } from 'ol/geom/Polygon';
+import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
+import { fromLonLat } from 'ol/proj';
+import OSM from 'ol/source/OSM';
+import VectorSource from 'ol/source/Vector';
+import { Fill, Icon, Style } from 'ol/style';
+import kompas from 'kompas';
+import './style.css';
+
+const main = () => {
+  const map = new Map({
+    target: 'map',
+    layers: [
+      new TileLayer({
+        source: new OSM(),
+      }),
+    ],
+    view: new View({
+      center: fromLonLat([0, 0]),
+      zoom: 10,
+    }),
+  });
+
+  const source = new VectorSource();
+  const layer = new VectorLayer({
+    source: source,
+  });
+
+  map.addLayer(layer);
+
+  navigator.geolocation.watchPosition(
+    function (pos) {
+      const coords = [pos.coords.longitude, pos.coords.latitude];
+      const accuracy = circular(coords, pos.coords.accuracy);
+      source.clear(true);
+      source.addFeatures([
+        new Feature(
+          accuracy.transform('EPSG:4326', map.getView().getProjection()),
+        ),
+        new Feature(new Point(fromLonLat(coords))),
+      ]);
+    },
+    function (error) {
+      alert(`ERROR: ${error.message}`);
+    },
+    {
+      enableHighAccuracy: true,
+    },
+  );
+
+  const locate = document.createElement('div');
+  locate.className = 'ol-control ol-unselectable locate';
+  locate.innerHTML = '<button title="Locate me">◎</button>';
+  locate.addEventListener('click', function () {
+    if (!source.isEmpty()) {
+      map.getView().fit(source.getExtent(), {
+        maxZoom: 18,
+        duration: 500,
+      });
+    }
+  });
+
+  map.addControl(
+    new Control({
+      element: locate,
+    }),
+  );
+
+
+
+  const style = new Style({
+    fill: new Fill({
+      color: 'rgba(0, 0, 255, 0.2)',
+    }),
+    image: new Icon({
+      src: './data/location-heading.svg',
+      rotateWithView: true,
+    }),
+  });
+
+  layer.setStyle(style);
+
+
+  function startCompass() {
+    kompas()
+      .watch()
+      .on('heading', function (heading) {
+        style.getImage().setRotation((Math.PI / 180) * heading);
+      });
+  }
+
+  if (
+    window.DeviceOrientationEvent &&
+    typeof DeviceOrientationEvent.requestPermission === 'function'
+  ) {
+    locate.addEventListener('click', function () {
+      DeviceOrientationEvent.requestPermission()
+        .then(startCompass)
+        .catch(function (error) {
+          alert(`ERROR: ${error.message}`);
+        });
+    });
+  } else if ('ondeviceorientationabsolute' in window) {
+    startCompass();
+  } else {
+    alert('No device orientation provided by device');
+  }
+
+
+  ///////
+
+  navigator.geolocation.getCurrentPosition(
+    function (pos) {
+      const coords = [pos.coords.longitude, pos.coords.latitude];
+      map.getView().setCenter(fromLonLat(coords));
+      map.getView().setZoom(18);
+    },
+    function (error) {
+      alert(`ERROR: ${error.message}`);
+    },
+    {
+      enableHighAccuracy: true,
+    },
+  );
+};
+
+window.addEventListener('load', main);
